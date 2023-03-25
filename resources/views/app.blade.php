3021bb0a152f9fd7ed6251c44c788dcf77c59416
codegeassj6 <jhonreyrepuelaj6.com>
2023-03-25 15:49:12 +0800

    cache_clear

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="shortcut icon" href="#">
     </head>
     <body>
 
@@ -14,8 +15,8 @@
             <app-component></app-component>
         </div>
 
-        {{-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5828491790124517"
-     crossorigin="anonymous"></script> --}}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5828491790124517"
+     crossorigin="anonymous"></script>
 
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
