25ca7639533a183376d523f87a0f180d107b4fe6
codegeassj6 <jhonreyrepuelaj6@gmail.com>
2023-04-18 23:18:08 +0800

    clar

--- public/js/Store.js
+++ public/js/Store.js
@@ -461,7 +461,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.product-img[data-v-0d00f4be] {\r\n    height: 190px;\n}\r\n\r\n\r\n\r\n\r\n\r\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.product-img[data-v-0d00f4be] {\n    height: 190px;\n}\n\n\n\n\n\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
