973c6f092181acf0faa9508a1cd66738551e922c
codegeassj6 <jhonreyrepuelaj6@gmail.com>
2023-05-28 05:04:26 +0800

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -103,7 +103,8 @@ class AuthController extends Controller
     {
         return response()->json([
             'access_token' => $token,
-            'user' => $this->guard()->user(),
+            // 'user' => $this->guard()->user(),
+            'user' => Auth::user(),
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
         ]);
