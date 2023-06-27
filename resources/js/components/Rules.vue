291b28d53b662f8761ab59d6aec3a31865b011c7
codegeassj6 <jhonreyrepuelaj6@gmail.com>
2023-06-27 10:02:07 +0800

    z

--- resources/js/components/Rules.vue
+++ resources/js/components/Rules.vue
@@ -1,66 +1,153 @@
 <template>
-  <div class="">
+  <div class="space-intro">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9">
 
+
+          <div class="accordion" id="accordionPanelsStayOpenExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button
+                  class="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseOne"
+                  aria-expanded="true"
+                  aria-controls="panelsStayOpen-collapseOne"
+                >
+                  No Refund
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseOne"
+                class="accordion-collapse collapse show"
+              >
+                <div class="accordion-body">
+                  <ul>
+                    <li>
+                      Blackout / Brownout
+                    </li>
+                    <li>
+                      10 Pesos new variant that gives 40 minutes of time instead of 80 minutes.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button
+                  class="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="panelsStayOpen-collapseTwo"
+                >
+                  Reservation
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseTwo"
+                class="accordion-collapse collapse show"
+              >
+                <div class="accordion-body">
+                  <ul>
+                    <li>
+                      20 minutes expiration limit for reservation as long as there is still someone plays on that pc.
+                    </li>
+                    <li>
+                      Another reserver player can replace the other reserve player if the current player time is finish and the reserve player is not around
+                    </li>
+                    <li>
+                      If player wants to play while the specific station he wants
+                    </li>
+
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button
+                  class="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseThree"
+                  aria-expanded="false"
+                  aria-controls="panelsStayOpen-collapseThree"
+                >
+                  Accordion Item #3
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseThree"
+                class="accordion-collapse collapse show"
+              >
+                <div class="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classes that we use to style each element. These
+                  classes control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the
+                  <code>.accordion-body</code>, though the transition does limit
+                  overflow.
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+        <div class="col-md-3">s</div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-
-
 export default {
-    data() {
-        return {
+  data() {
+    return {};
+  },
+  components: {},
 
-        }
-    },
-    components: {
+  props: {},
 
-    },
+  computed: {},
 
-    props: {
+  methods: {},
 
-    },
-
-    computed: {
-
-    },
-
-    methods: {
-
+  watch: {
+    $data: {
+      handler: function (val, oldVal) {
+        console.log("watcher: ", val);
+      },
+      deep: true,
     },
 
-    watch: {
-        $data: {
-            handler: function(val, oldVal) {
-                console.log('watcher: ',val);
-            },
-            deep: true
-        },
-
-        $props: {
-            handler: function(val, oldVal) {
-                console.log('watcher: ',val);
-            },
-            deep: true
-        },
-        some_prop: function() {
-            //do something if some_prop updated
-        },
+    $props: {
+      handler: function (val, oldVal) {
+        console.log("watcher: ", val);
+      },
+      deep: true,
     },
-
-    updated() {
-
+    some_prop: function () {
+      //do something if some_prop updated
     },
+  },
 
-    beforeMounted() {
+  updated() {},
 
-    },
+  beforeMounted() {},
 
-    mounted() {
-
-    },
-}
+  mounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>
